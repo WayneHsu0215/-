b9593d4d21900b0f4b15ba6f02a7d36e987281b5
WayneHsu0215 <120437313+WayneHsu0215@users.noreply.github.com>
2023-04-17 17:31:10 +0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter1 from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+		adapter1: adapter1({
+			edge: true,
+			split: false
+		  }),
 	},
 	preprocess: [
 		preprocess({
