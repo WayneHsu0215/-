3f44e44782cd67fdfbe8f98dd1cd125277a56761
WayneHsu0215 <120437313+WayneHsu0215@users.noreply.github.com>
2023-04-17 17:37:42 +0800

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,16 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
-import adapter1 from '@sveltejs/adapter-netlify';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
-		adapter1: adapter1({
-			edge: true,
-			split: false
-		  }),
 	},
 	preprocess: [
 		preprocess({
 			postcss: true,
 		}),
 	],
-	
 };
 
 export default config;
